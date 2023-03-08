c793f2d21cfccb077fa162055a5f3003b09aa4df
yupei_lin <YuPei_Lin@asus.com>
2023-03-08 11:16:30 +0800

    Ethernet : remove EOPNOTSUPP to support systemctl suspend
    Change-Id: Ica6fdf61eb32af4c12a5f4097d2b418c2d2d10b5

--- drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_ethtool.c
@@ -665,9 +665,6 @@ static int stmmac_set_wol(struct net_device *dev, struct ethtool_wolinfo *wol)
 	struct stmmac_priv *priv = netdev_priv(dev);
 	u32 support = WAKE_MAGIC | WAKE_UCAST;
 
-	//RK3568 using wolirq_io to support wol function.
-	return -EOPNOTSUPP;
-
 	if (!device_can_wakeup(priv->device))
 		return -EOPNOTSUPP;
 
