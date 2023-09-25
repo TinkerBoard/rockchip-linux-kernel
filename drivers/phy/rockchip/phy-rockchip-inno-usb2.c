TinkerBoard/rockchip-linux-kernel

commit ea6c711e1832e2f6b5f4d358f0a0d60f9f814461

usb: enable usb remote wakeup
Change-Id: Ifbde804c2fe663dad7a263ac0d5392d06dae7b9f

drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -3512,7 +3512,7 @@ static const struct rockchip_usb2phy_cfg rk3568_phy_cfgs[] = {
 			},
 			[USB2PHY_PORT_HOST] = {
 				/* Select suspend control from controller */
-				.phy_sus	= { 0x0004, 8, 0, 0x1d2, 0x1d2 },
+				.phy_sus        = { 0x0004, 8, 0, 0x1d2, 0x1d1 },
 				.ls_det_en	= { 0x0080, 1, 1, 0, 1 },
 				.ls_det_st	= { 0x0084, 1, 1, 0, 1 },
 				.ls_det_clr	= { 0x0088, 1, 1, 0, 1 },

arch/arm64/boot/dts/rockchip/rk3568-tinker_board_3-common.dtsi
@@ -929,7 +929,7 @@
 				regulator-max-microvolt = <900000>;
 				regulator-name = "vdda_0v9";
 				regulator-state-mem {
-					regulator-off-in-suspend;
+					regulator-on-in-suspend;
 				};
 			};
 
@@ -986,7 +986,7 @@
 				regulator-max-microvolt = <1800000>;
 				regulator-name = "vcca_1v8";
 				regulator-state-mem {
-					regulator-off-in-suspend;
+					regulator-on-in-suspend;
 				};
 			};
 
@@ -1029,7 +1029,7 @@
 				regulator-boot-on;
 				regulator-name = "vcc_3v3";
 				regulator-state-mem {
-					regulator-off-in-suspend;
+					regulator-on-in-suspend;
 				};
 			};
 

arch/arm64/boot/dts/rockchip/rk3568-tinker_board_3.dts
@@ -126,10 +126,15 @@
                 | RKPM_SLP_CENTER_OFF
                 | RKPM_SLP_HW_PLLS_OFF
                 | RKPM_SLP_PMUALIVE_32K
-                | RKPM_SLP_OSC_DIS
                 | RKPM_SLP_32K_PVTM
                 )
                 >;
+	rockchip,wakeup-config = <
+		(0
+		| RKPM_GPIO_WKUP_EN
+		| RKPM_USB_WKUP_EN
+		)
+		>;
 };
 
 &vop {

commit 49473e2a428681b43bb7d423c962098ce22f603b

usb: set USB2PHY_PORT_HOST phy_sus to 0x1d2
Set USB2PHY_PORT_HOST phy_sus to 0x1d1 will cause usb3 not work.

Fixes: ea6c711e1832 ("usb: enable usb remote wakeup")
Change-Id: Iff523f13234845a8577fbb15a8fa68640fc89e33

drivers/phy/rockchip/phy-rockchip-inno-usb2.c
@@ -3785,7 +3785,7 @@ static const struct rockchip_usb2phy_cfg rk3568_phy_cfgs[] = {
 			},
 			[USB2PHY_PORT_HOST] = {
 				/* Select suspend control from controller */
-				.phy_sus        = { 0x0004, 8, 0, 0x1d2, 0x1d1 },
+				.phy_sus        = { 0x0004, 8, 0, 0x1d2, 0x1d2 },
 				.ls_det_en	= { 0x0080, 1, 1, 0, 1 },
 				.ls_det_st	= { 0x0084, 1, 1, 0, 1 },
 				.ls_det_clr	= { 0x0088, 1, 1, 0, 1 },